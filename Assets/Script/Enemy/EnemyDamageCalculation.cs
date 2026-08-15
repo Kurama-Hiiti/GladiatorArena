037e671ad61c5b2ee6d144fbf2e61ac3e503eaf3
Kurama-Hiiti <kurama.hiiti.pentas.game@gmail.com>
2026-08-15 18:06:00 +0900

    コメント追加

--- Assets/Script/Enemy/EnemyDamageCalculation.cs
+++ Assets/Script/Enemy/EnemyDamageCalculation.cs
@@ -6,6 +6,7 @@ public class EnemyDamageCalculation : MonoBehaviour
 {
     //敵がプレイヤーに攻撃するときのダメージ処理関数(敵の攻撃判定の場所にアタッチ)
 
+    //敵の強攻撃の場合の攻撃倍率
     private float SkillDamageRate = 1.2f;
 
     [SerializeField]
@@ -14,14 +15,18 @@ public class EnemyDamageCalculation : MonoBehaviour
     [SerializeField]
     private EnemyManager enemyManager;
 
+    //ダメージ計算関数
     public int PlayerTakeDamage(int playerDefensePower)
     {
         float damage = 0;
 
+        //現在のアニメーションを取得
         AnimatorClipInfo clipInfo = animator.GetCurrentAnimatorClipInfo(0)[0];
 
+        //アニメーションの名前を取得
         string animationName = clipInfo.clip.name;
 
+        //アニメーションの名前で場合分け
         if (animationName == "SkillAttack")
         {
             damage = (enemyManager.enemyAttackPower * SkillDamageRate) * (100f / (100f + playerDefensePower));
